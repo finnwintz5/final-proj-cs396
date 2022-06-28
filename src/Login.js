f2585601737ef679070bfe2e843c05214c00f1b1
Finn Wintz <finnwintz@macbook-pro-72.myfiosgateway.com>
2022-06-27 23:14:36 -0400

    second try

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { Link } from "react-router-dom"; 
+import {hasCsrfToken, setAccessTokenCookie} from './utils';
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        }
+        this.handleLogin = this.handleLogin.bind(this);
+        this.handleUserChange = this.handleUserChange.bind(this);
+        this.handlePassChange = this.handlePassChange.bind(this);
+    }
+    
     componentDidMount() {
 
+    }
+
+    handleLogin () {
+        setAccessTokenCookie(this.state.username, this.state.username);
+    }
+
+    handleUserChange(event) {
+        this.setState({username: event.target.value});
+    }
+
+    handlePassChange(event) {
+        this.setState({password: event.target.value});
     }
 
     render () {
@@ -15,13 +40,23 @@ class Login extends React.Component {
                 <form method="POST" action="/login">
                     <div>
                         <label htmlFor="username">Username</label>
-                        <input name="username" type="text" id="username" placeholder="username"/> 
+                        <input name="username"
+                                type="text"
+                                id="username"
+                                placeholder="username"
+                                onClick={this.handleUserChange()}
+                                onKeyDown={this.handleUserChange()}/> 
                     </div>
                     <div>
                         <label htmlFor="password">Password</label>  
-                        <input name="password" type="password" id="password" placeholder="password"/>
+                        <input name="password"
+                                type="password"
+                                id="password"
+                                placeholder="password"
+                                onClick={this.handlePassChange()}
+                                onKeyDown={this.handlePassChange()}/>
                     </div>
-                    <button type="submit">Login</button>
+                    <Link to="/" onClick={this.handleLogin()}>Login</Link>
                 </form>
             </div>
         
