c84f24c8cacb382e3bcf7dbbf2f542b0f026332a
Finn Wintz <finnwintz@macbook-pro-72.myfiosgateway.com>
2022-06-28 20:54:27 -0400

    second try 10

--- src/utils.js
+++ src/utils.js
@@ -22,6 +22,8 @@ function setCookie(name, val) {
   }
 
 export function setAccessTokenCookie(username, password, callback) {
+    console.log("username" + username);
+    console.log("password" + password);
     const postData = {
         "username": username,
         "password": password
@@ -36,7 +38,7 @@ export function setAccessTokenCookie(username, password, callback) {
         .then(response => response.json())
         .then(data => {
             const token = data.access_token;
-            console.log(token);
+            console.log("token" + token);
             setCookie('access_token_cookie_js', token);
             callback();
         });
